ee9c3ad2dd2b3c62aeccd89d3a2cef586b8ccd43
Oliver Millin <omillin@ou.edu>
2022-07-12 12:40:04 -0500

    added pt anomalies

--- main/Reanalysis_Plots/Theta_2PVU/ERA5/theta_2021.py
+++ main/Reanalysis_Plots/Theta_2PVU/ERA5/theta_2021.py
@@ -140,3 +140,107 @@ cbar = fig.colorbar(cs, cax=cb_ax,orientation="horizontal",ticks= np.arange(270,
 cbar.set_label("Theta (C)", fontsize = 17)
 cbar.ax.tick_params(labelsize=15)
 plt.savefig("/share/data1/Students/ollie/CAOs/project-2021-cao/main/Reanalysis_Plots/Theta_2PVU/ERA5/pt_2021.png", bbox_inches = 'tight', dpi = 500)
+
+# Now read in the ltm.
+# Go to directory with the data in.
+path = '/share/data1/Students/ollie/CAOs/Data/Feb_2021_CAO/ERA5/theta/Climo_Data'
+dir = os.chdir(path)
+
+nc = Dataset('theta_feb_ltm.nc', 'r')
+theta_ltm = nc.variables['pt'][:, lat_ind1:lat_ind2+1, :]
+time_ltm = num2date(nc.variables['time'][:], nc.variables['time'].units, nc.variables['time'].calendar, only_use_cftime_datetimes = False, only_use_python_datetimes = True)
+nc.close()
+
+# Now create a day tracker.
+days_ltm = np.array([d.day for d in time_ltm])
+
+# Now find time indexes for each period.
+ltm_time_ind1, ltm_time_ind2, ltm_time_ind3, ltm_time_ind4 = np.where(days_ltm == date1.day)[0][0], np.where(days_ltm == date2.day)[0][0], np.where(days_ltm == date3.day)[0][0], np.where(days_ltm == date4.day)[0][0]
+
+# Calculate anomalies.
+period1_anom = pt_period1 - theta_ltm[ltm_time_ind1:ltm_time_ind2+1]
+period2_anom = pt_period2 - theta_ltm[ltm_time_ind3:ltm_time_ind4+1]
+
+# Now plot!
+# Normalized color map.
+clevs = np.arange(-30, 32, 2)
+my_cmap, norm = NormColorMap('RdBu_r', clevs)
+
+# Make figure.
+# Fig dimensions:
+nrows = 2
+ncols = 3
+fig_no = np.arange(1, 9, 1)
+labels = ['a)', 'b)', 'c)', 'd)', 'e)', 'f)']
+fig = plt.figure(figsize=(12, 9))
+for i in range(len(time_period1)):
+    if i == 0:
+        pt_shifted, lons_shifted = addcyclic(period1_anom[i], lon_region)
+        lons, lats = np.meshgrid(lons_shifted, lat_region)
+
+        ax = fig.add_subplot(nrows, ncols, fig_no[i])
+        # Basemap plot.
+        map = LambConfMap(width = 6000000, height = 5100000, area_thresh = 1000.0, lat_range = [89.5, 60], center_range = [70.0, 200])
+
+        x, y = map(lons, lats)
+
+        # Contourf.
+        cs = map.contourf(x, y, pt_shifted, clevs, extend='both', norm = norm, cmap = my_cmap)
+        p1, p2, p3, p4 = DrawPolygon(map, lat_range = [85.5, 66], lon_range = [111, 187.5], grid_space = 0.5, lw = 3, color = 'black')
+        text = ax.text(-500000,2500000,"Wave Break 1", size=15, verticalalignment='center', rotation=90., weight = 'bold')
+        ax.set_title(f"{labels[i]} {time_period1[i].strftime('%Y/%m/%d')}", fontsize = 16, weight = 'bold')
+        plt.tight_layout()
+    else:
+        pt_shifted, lons_shifted = addcyclic(period1_anom[i], lon_region)
+        lons, lats = np.meshgrid(lons_shifted, lat_region)
+
+        ax = fig.add_subplot(nrows, ncols, fig_no[i])
+        # Basemap plot.
+        map = LambConfMap(width = 6000000, height = 5100000, area_thresh = 1000.0, lat_range = [89.5, 60], center_range = [70.0, 200])
+
+        x, y = map(lons, lats)
+
+        # Contourf.
+        cs = map.contourf(x, y, pt_shifted, clevs, extend='both', norm = norm, cmap = my_cmap)
+        p1, p2, p3, p4 = DrawPolygon(map, lat_range = [85.5, 66], lon_range = [111, 187.5], grid_space = 0.5, lw = 3, color = 'black')
+        ax.set_title(f"{labels[i]} {time_period1[i].strftime('%Y/%m/%d')}", fontsize = 16, weight = 'bold')
+        plt.tight_layout()
+
+for i in range(len(time_period2)):
+    if i == 0:
+        pt_shifted, lons_shifted = addcyclic(period2_anom[i], lon_region)
+        lons, lats = np.meshgrid(lons_shifted, lat_region)
+
+        ax = fig.add_subplot(nrows, ncols, fig_no[i+3])
+        # Basemap plot.
+        map = LambConfMap(width = 5000000, height = 4100000, area_thresh = 1000.0, lat_range = [70, 40], center_range = [57, 310])
+
+        x, y = map(lons, lats)
+
+        # Contourf.
+        cs = map.contourf(x, y, pt_shifted, clevs, extend='both', norm = norm, cmap = my_cmap)
+        p1, p2, p3, p4 = DrawPolygon(map, lat_range = [60, 48], lon_range = [291, 324], grid_space = 0.5, lw = 3, color = 'black')
+        ax.set_title(f"{labels[i+3]} {time_period2[i].strftime('%Y/%m/%d')}", fontsize = 16, weight = 'bold')
+        text = ax.text(-400000,2100000,"Wave Break 2", size=15, verticalalignment='center', rotation=90., weight = 'bold')
+        plt.tight_layout()
+    else:
+        pt_shifted, lons_shifted = addcyclic(period2_anom[i], lon_region)
+        lons, lats = np.meshgrid(lons_shifted, lat_region)
+
+        ax = fig.add_subplot(nrows, ncols, fig_no[i+3])
+        # Basemap plot.
+        map = LambConfMap(width = 5000000, height = 4100000, area_thresh = 1000.0, lat_range = [70, 40], center_range = [57.0, 310])
+
+        x, y = map(lons, lats)
+
+        # Contourf.
+        cs = map.contourf(x, y, pt_shifted, clevs, extend='both', norm = norm, cmap = my_cmap)
+        p1, p2, p3, p4 = DrawPolygon(map, lat_range = [60, 48], lon_range = [291, 324], grid_space = 0.5, lw = 3, color = 'black')
+        ax.set_title(f"{labels[i+3]} {time_period2[i].strftime('%Y/%m/%d')}", fontsize = 16, weight = 'bold')
+        plt.tight_layout()
+
+cb_ax = fig.add_axes([0.05, -0.01, 0.91, 0.04])
+cbar = fig.colorbar(cs, cax=cb_ax,orientation="horizontal",ticks = np.arange(-30, 40, 10),extend="both")
+cbar.set_label("Theta Anomaly (C)", fontsize = 17)
+cbar.ax.tick_params(labelsize=15)
+plt.savefig("/share/data1/Students/ollie/CAOs/project-2021-cao/main/Reanalysis_Plots/Theta_2PVU/ERA5/pt_2021_anom.png", bbox_inches = 'tight', dpi = 500)
